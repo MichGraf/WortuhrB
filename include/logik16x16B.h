4022764e9f5b0a79af5d2857a39bc06266092292
Michael Graf <michael.graf@gmx.de>
2025-10-19 17:49:18 +0200

    zwei getrennte switch und "kurz vor/nach)

--- include/logik16x16B.h
+++ include/logik16x16B.h
@@ -37,7 +37,7 @@ if (enable_es_ist==1) {Set_Text(w_es,h,s,v); Set_Text(w_ist,h,s,v);}
 if (digitalRead(praesenz)!=0) {Set_Text(w_hallo,30,150,v/2); Set_Text(w_brigitte,30,150,v/2); if (variante==0) Set_Text(w_liebe,30,150,v/2); }
 
 
-
+if (variante==0) {
 switch(tm.tm_min) {
 
 case 0: Set_Text(w_uhr,h,s,v); break;
@@ -45,12 +45,12 @@ case 1: Set_Text(w_einemin,h,s,v); Set_Text(w_minute,h,s,v); Set_Text(w_nach,h,s
 case 2: Set_Text(w_zweimin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
 case 3: Set_Text(w_dreimin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
 case 4: Set_Text(w_viermin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
-case 5: Set_Text(w_fuenfmin,h,s,v); if (variante==0) Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 5: Set_Text(w_fuenfmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
 case 6: Set_Text(w_sechsmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
 case 7: Set_Text(w_siebenmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
 case 8: Set_Text(w_achtmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
 case 9: Set_Text(w_neunmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
-case 10: Set_Text(w_zehnmin,h,s,v); if (variante==0) Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 10: Set_Text(w_zehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
 case 11: Set_Text(w_elfm,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
 case 12: Set_Text(w_zwoelfmin,h,s,v); Set_Text(w_nach,h,s,v); break;
 case 13: Set_Text(w_dreizehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); break;
@@ -60,12 +60,12 @@ case 16: Set_Text(w_sechzehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_na
 case 17: Set_Text(w_siebzehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
 case 18: Set_Text(w_achtzehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
 case 19: Set_Text(w_neunzehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
-case 20: Set_Text(w_zehnmin,h,s,v); if (variante==0) Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 20: Set_Text(w_zehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 21: Set_Text(w_neunmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 22: Set_Text(w_achtmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 23: Set_Text(w_siebenmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 24: Set_Text(w_sechsmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
-case 25: Set_Text(w_fuenfmin,h,s,v); if (variante==0) Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 25: Set_Text(w_fuenfmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 26: Set_Text(w_viermin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 27: Set_Text(w_dreimin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 28: Set_Text(w_zweimin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
@@ -75,12 +75,12 @@ case 31: Set_Text(w_einemin,h,s,v); Set_Text(w_minute,h,s,v); Set_Text(w_nach,h,
 case 32: Set_Text(w_zweimin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 33: Set_Text(w_dreimin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 34: Set_Text(w_viermin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
-case 35: Set_Text(w_fuenfmin,h,s,v); if (variante==0) Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 35: Set_Text(w_fuenfmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 36: Set_Text(w_sechsmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 37: Set_Text(w_siebenmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 38: Set_Text(w_achtmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 39: Set_Text(w_neunmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
-case 40: Set_Text(w_zehnmin,h,s,v); if (variante==0) Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 40: Set_Text(w_zehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 41: Set_Text(w_elfm,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 42: Set_Text(w_zwoelfmin,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
 case 43: Set_Text(w_dreizehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
@@ -91,18 +91,88 @@ case 46: Set_Text(w_vierzehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vo
 case 47: Set_Text(w_dreizehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
 case 48: Set_Text(w_zwoelfmin,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
 case 49: Set_Text(w_elfm,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
-case 50: Set_Text(w_zehnmin,h,s,v); if (variante==0) Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
+case 50: Set_Text(w_zehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
 case 51: Set_Text(w_neunmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
 case 52: Set_Text(w_achtmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
 case 53: Set_Text(w_siebenmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
 case 54: Set_Text(w_sechsmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
-case 55: Set_Text(w_fuenfmin,h,s,v); if (variante==0) Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
+case 55: Set_Text(w_fuenfmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
 case 56: Set_Text(w_viermin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
 case 57: Set_Text(w_dreimin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
 case 58: Set_Text(w_zweimin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
 case 59: Set_Text(w_einemin,h,s,v); Set_Text(w_minute,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
 
 }
+} else {
+
+switch(tm.tm_min) {
+
+case 0: Set_Text(w_uhr,h,s,v); break;
+case 1: 
+case 2:
+case 3:
+case 4: Set_Text(w_kurz,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 5: Set_Text(w_fuenfmin,h,s,v);  Set_Text(w_nach,h,s,v); break;
+case 6: Set_Text(w_sechsmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 7: Set_Text(w_siebenmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 8: Set_Text(w_achtmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 9: Set_Text(w_neunmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 10: Set_Text(w_zehnmin,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 11: Set_Text(w_elfm,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 12: Set_Text(w_zwoelfmin,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 13: Set_Text(w_dreizehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); break;
+case 14: Set_Text(w_vierzehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 15: Set_Text(w_viertelm,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 16: Set_Text(w_sechzehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 17: Set_Text(w_siebzehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 18: Set_Text(w_achtzehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 19: Set_Text(w_neunzehnmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); break;
+case 20: Set_Text(w_zehnmin,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 21: Set_Text(w_neunmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 22: Set_Text(w_achtmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 23: Set_Text(w_siebenmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 24: Set_Text(w_sechsmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 25: Set_Text(w_fuenfmin,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 26: 
+case 27: 
+case 28: 
+case 29: Set_Text(w_kurz,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 30: Set_Text(w_halb,h,s,v); stunde++; break;
+case 31: 
+case 32: 
+case 33: 
+case 34: Set_Text(w_kurz,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 35: Set_Text(w_fuenfmin,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 36: Set_Text(w_sechsmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 37: Set_Text(w_siebenmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 38: Set_Text(w_achtmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 39: Set_Text(w_neunmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 40: Set_Text(w_zehnmin,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_halb,h,s,v); stunde++; break;
+case 41: 
+case 42: 
+case 43: 
+case 44: Set_Text(w_kurz,h,s,v); Set_Text(w_vor,h,s,v); Set_Text(w_dreiviertel,h,s,v); stunde++; break;
+case 45: if (variante==0) {Set_Text(w_viertelm,h,s,v); Set_Text(w_vor,h,s,v); stunde++;}
+         else {Set_Text(w_dreiviertel,h,s,v); stunde++;} break;
+case 46: 
+case 47:
+case 48:
+case 49: Set_Text(w_kurz,h,s,v); Set_Text(w_nach,h,s,v); Set_Text(w_dreiviertel,h,s,v); stunde++; break;
+case 50: Set_Text(w_zehnmin,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
+case 51: Set_Text(w_neunmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
+case 52: Set_Text(w_achtmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
+case 53: Set_Text(w_siebenmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
+case 54: Set_Text(w_sechsmin,h,s,v); Set_Text(w_minuten,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
+case 55: Set_Text(w_fuenfmin,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
+case 56: 
+case 57:
+case 58:
+case 59: Set_Text(w_kurz,h,s,v); Set_Text(w_vor,h,s,v); stunde++; break;
+
+}
+}
+
+
 
 
 if (stunde>12) stunde=stunde-12;
@@ -110,7 +180,7 @@ if (stunde>12) stunde=stunde-12;
 switch(stunde) {
 
 case 0: Set_Text(w_zwoelf,h,s,v); break;
-case 1: if (tm.tm_min==58 || tm.tm_min==59 || (tm.tm_min>=0 && tm.tm_min<=2)) Set_Text(w_ein,h,s,v); else Set_Text(w_eins,h,s,v); break;
+case 1: if (tm.tm_min==0) Set_Text(w_ein,h,s,v); else Set_Text(w_eins,h,s,v); break;
 case 2: Set_Text(w_zwei,h,s,v); break;
 case 3: Set_Text(w_drei,h,s,v); break;
 case 4: Set_Text(w_vier,h,s,v); break;
